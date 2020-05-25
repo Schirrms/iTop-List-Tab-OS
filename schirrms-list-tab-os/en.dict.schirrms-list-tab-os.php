46d43db4e204e141b8ee451dcda95f14fd305f52
Schirrms <schirrms@schirrms.net>
2020-05-25 11:27:18 +0200

    Add text definitions (0.0.1.3)

--- schirrms-list-tab-os/en.dict.schirrms-list-tab-os.php
+++ schirrms-list-tab-os/en.dict.schirrms-list-tab-os.php
@@ -8,5 +8,11 @@
 
 Dict::Add('EN US', 'English', 'English', array(
 	// Dictionary entries go here
+	'Class:OSVersion/Attribute:serverosversions_list' => 'Server using it',
+	'Class:OSVersion/Attribute:virtualdeviceosversions_list' => 'VM using it',
+	'Class:OSVersion/Attribute:connectableciosversions_list' => 'Connectable CIs using it',
+	'Class:OSVersion/Attribute:ospatchosversions_list' => 'OS Patches using it',
+	'Class:OSVersion/Attribute:oslicenceosversions_list' => 'OS Licences using it',
+	'Class:OSFamily/Attribute:osfamilys_list' => 'OS Versions',
 ));
 ?>
