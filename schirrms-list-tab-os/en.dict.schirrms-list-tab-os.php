396d473510eb31f30c6d01047f811e35cdb6c961
Schirrms <schirrms@schirrms.net>
2020-05-25 11:40:37 +0200

    Debug ends ? (0.0.1.4)

--- schirrms-list-tab-os/en.dict.schirrms-list-tab-os.php
+++ schirrms-list-tab-os/en.dict.schirrms-list-tab-os.php
@@ -12,7 +12,7 @@ Dict::Add('EN US', 'English', 'English', array(
 	'Class:OSVersion/Attribute:virtualdeviceosversions_list' => 'VM using it',
 	'Class:OSVersion/Attribute:connectableciosversions_list' => 'Connectable CIs using it',
 	'Class:OSVersion/Attribute:ospatchosversions_list' => 'OS Patches using it',
-	'Class:OSVersion/Attribute:oslicenceosversions_list' => 'OS Licences using it',
+	'Class:OSVersion/Attribute:oslicenceosversions_list' => 'OS Licenses using it',
 	'Class:OSFamily/Attribute:osfamilys_list' => 'OS Versions',
 ));
 ?>
---
+++ schirrms-list-tab-os/fr.dict.schirrms-list-tab-os.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	'Class:OSVersion/Attribute:serverosversions_list' => 'Serveur l\'utilisant',
+	'Class:OSVersion/Attribute:virtualdeviceosversions_list' => 'VM l\'utilisant',
+	'Class:OSVersion/Attribute:connectableciosversions_list' => 'CIs connectés l\'utilisant',
+	'Class:OSVersion/Attribute:ospatchosversions_list' => 'Patches OS l\'utilisant',
+	'Class:OSVersion/Attribute:oslicenceosversions_list' => 'Licences OS l\'utilisant',
+	'Class:OSFamily/Attribute:osfamilys_list' => 'OS Versions',
+));
+?>
